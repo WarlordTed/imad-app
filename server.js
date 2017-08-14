08abd6fad5177bc7e8018eb8cd793a3951e3e605
WarlordTed <30731593+WarlordTed@users.noreply.github.com>
2017-08-14 21:59:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One By PRANAV JAIN',
+    heading: 'Article One',
+    date: 'Aug 18, 2017',
+    content: ` <p>
+                    Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One.
+                </p>
+                <p>
+                    Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One.
+                </p>
+                <p>
+                    Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One. Hello, this is my Article One.
+                </p> `
+};
+
+function createtemplate (data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate = 
+`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+    </head>
+    <body>
+        <div class='container'>
+            <div>
+                <a href='/'> Home</a>
+                <link href="/ui/style.css" rel="stylesheet" />
+                <h1>
+                    ${heading}
+                </h1>
+                <hr/>
+                <div>
+                    ${date}
+                </div>
+                ${content}
+            </div>  
+        </div>
+    </body>
+</html>
+`;
+return(htmltemplate);
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res) {
