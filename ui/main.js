68e83d0f1f2aa58336d846cf1639a4858db87ef0
WarlordTed <30731593+WarlordTed@users.noreply.github.com>
2017-09-03 12:04:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 
 var img = document.getElementById('Kakashi');
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
